af836c1bd86bb17c30d05041d731bfc984091e24
ISAAC_ABEL_CHAVEZ_NOLASCO <a.21308051280336@cbtis128.edu.mx>
2024-05-01 17:10:36 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,148 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(MyApp());
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //https://source.unsplash.com/random
+
+  File file = File("Users/user/Downloads/xs.jpg");
+
+  Uint8List? imageByteData;
+
+  void _getImage() async {
+    Uint8List data = (await rootBundle.load('Users/user/Downloads/xs.jpg'))
+        .buffer
+        .asUint8List();
+
+    setState(() {
+      print(data);
+      imageByteData = data;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getImage();
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "TELCEL",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Telcel'),
+        ),
+        body: Container(
+          color: Colors.grey,
+          child: Image.asset(
+            'assets/tela.png',
+            width: 300,
+            height: 300,
+            fit: BoxFit.contain,
+            frameBuilder: (context, child, frame, loaded) {
+              if (loaded) {
+                return child;
+              }
+              return AnimatedOpacity(
+                child: child,
+                opacity: frame == null ? 0 : 1,
+                duration: Duration(seconds: 1),
+                curve: Curves.easeOut,
+              );
+            },
+            errorBuilder: (context, error, stackTrace) {
+              return Center(
+                child: Text('Imagen no encontrada'),
+              );
+            },
+          ),
+
+          //child: Image.network(
+          //'https://source.unsplash.com/random',
+          //width: 300,
+          //height: 300,
+          //fit: BoxFit.contain,
+          //frameBuilder: (context, child, frame, loaded) {
+          //if (loaded) {
+          //return child;
+          //}
+          //return AnimatedOpacity(
+          //child: child,
+          //opacity: frame == null ? 0 : 1,
+          //duration: Duration(seconds: 1),
+          //curve: Curves.easeOut,
+          //);
+          //},
+          //errorBuilder: (context, error, stackTrace) {
+          //return Center(
+          //child: Text('Imagen no encontrada'),
+          //);
+          //},
+          //),
+
+          //child: Image.file(
+          //file,
+          //width: 300,
+          //height: 300,
+          //fit: BoxFit.contain,
+          //frameBuilder: (context, child, frame, loaded) {
+          //if (loaded) {
+          //return child;
+          //}
+          //return AnimatedOpacity(
+          //child: child,
+          //opacity: frame == null ? 0 : 1,
+          //duration: Duration(seconds: 1),
+          //curve: Curves.easeOut,
+          //);
+          //},
+          //errorBuilder: (context, error, stackTrace) {
+          //return Center(
+          //child: Text('Imagen no encontrada'),
+          //);
+          //},
+          //),
+
+          //child: Image.memory(
+          //imageByteData!,
+          //width: 300,
+          //height: 300,
+          //fit: BoxFit.contain,
+          //frameBuilder: (context, child, frame, loaded) {
+          //if (loaded) {
+          //return child;
+          //}
+          //return AnimatedOpacity(
+          //child: child,
+          //opacity: frame == null ? 0 : 1,
+          //duration: Duration(seconds: 1),
+          //curve: Curves.easeOut,
+          //);
+          //},
+          //errorBuilder: (context, error, stackTrace) {
+          //return Center(
+          //child: Text('Imagen no encontrada'),
+          //);
+          //},
+          //),
         ),
       ),
     );
